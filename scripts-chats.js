5ea0b22b66544aeb0923050b6bd85d7c11899a91
Ravi Choudhary <151042374+jatravi@users.noreply.github.com>
2024-12-07 12:47:18 +0530

    Added Gemini API

--- scripts-chats.js
+++ scripts-chats.js
@@ -1,6 +1,38 @@
 let prompt = document.querySelector('#prompt');
 let chatContainer = document.querySelector('.chat-container');
 
+let user={
+    data:null,
+}
+
+const Api_URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyABGS3fgQJ0kEDkgCo53uqtIC-yLnWbV8E"
+
+async function generateResponse(aiChatBox) {
+    let text=aiChatBox.querySelector(".ai-chat-area")
+    let RequestOption={
+        method: "POST",
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "contents": [{
+              "parts":[{"text": user.data}]
+              }]
+            })
+    }
+
+    try{
+        let response = await fetch(Api_URL, RequestOption);
+        let data = await response.json();
+        let apiResponse=data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim()
+        text.innerHTML=apiResponse
+    }
+    catch(error){
+        console.error(error);
+    }
+    finally{
+        chatContainer.scrollTo({top:chatContainer.scrollHeight,behavior:"smooth"})
+    }
+}
+
 function createChatBox(html,classes){
     let div=document.createElement("div")
     div.innerHTML=html
@@ -9,22 +41,27 @@ function createChatBox(html,classes){
 }
 
 function handlechatResponse(message){
+    user.data=message
     let html=`<img src="user.png" alt="User" width="30" id="UserImage">
 <div class="user-chat-area">
-${message}
+${user.data}
 </div>`
 prompt.value=""
 
 let userChatBox = createChatBox(html,"user-chat")
 chatContainer.appendChild(userChatBox)
+chatContainer.scrollTo({top:chatContainer.scrollHeight,behavior:"smooth"})
 
 setTimeout(()=>{
     let html=`<img src="ai.png" alt="AI" width="45" id="AIImage">
         <div class="ai-chat-area">
-        ${message}
+        <img src="LOADING.webp" 
+                alt="LOADING-DARK.webp" 
+                class="load" width="35px">
         </div>`
     let aiChatBox = createChatBox(html,"ai-chat")
     chatContainer.appendChild(aiChatBox)
+    generateResponse(aiChatBox)
 },600)
 }
 
